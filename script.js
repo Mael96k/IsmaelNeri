252a3b8273fd2e1621242de5087a28ed53877555
Mael96k <ismaelnere96@gmail.com>
2021-12-02 08:01:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector(".linhas_menu").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
+    document.querySelector(".container").classList.toggle("show_menu")
 );
