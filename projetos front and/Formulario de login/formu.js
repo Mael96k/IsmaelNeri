ac2adc44e92e174c922c1197fe0159751cac8917
Mael96k <ismaelnere96@gmail.com>
2021-12-13 18:35:41 -0300

    Update formu.js

--- projetos front and/Formulario de login/formu.js
+++ projetos front and/Formulario de login/formu.js
@@ -2,7 +2,9 @@ class validator{
 
     constructor() {
         this.validations = [
-            "data-min-length",
+            `data-min-length`,
+            `data-max-length`,
+            `data-only-letters`,
         ]
     }
 
@@ -10,13 +12,20 @@ class validator{
     // Iniciar a validação de todos os campos
     Validate(form){
 
+        // resgata todas as validações
+        let currentValidations = document.querySelectorAll(`form .error-validation`);
+
+        if(currentValidations.length){
+            this.cleanValidations(currentValidations);
+        }
+
         // Pegar os inputs
-        let inputs = form.getElementsByTagName("input");
-        
+        let inputs = form.getElementsByTagName(`input`);
         // Transformo uma HTMLCollection -> array
         let inputsArray = [...inputs];
+
         // Loop nos inputs e validação meadiante ao que for encontrado
-    inputsArray.forEach(function(input) {
+    inputsArray.forEach(function(input, obj) {
 
         // Loop em todas as validações existentes
         for(let i = 0; this.validations.length > i; i++){
@@ -27,7 +36,11 @@ class validator{
                 // limpando a string para virar um método
                 let method = this.validations[i].replace("data-","").replace("","");
 
-                // valor 
+                // valor do input
+                let valeu = input.getAttribute(this.validations[i]);
+
+                // invocar o método 
+                this[method](input, value);
 
             }
         }
@@ -36,10 +49,60 @@ class validator{
     }
 
     //verifica se um input tem um número mínimo de caracteres
-    minlength(){
+    minlength(input, minValue){
 
+        let inputLength = input.value.length;
+
+        let errorMessage = `o campo precisa ter pelo menos ${minValue} caracteres`;
+
+        if (inputLength < minValue) {
+            this.printMessage(input, errorMessage);
+        }
+
+    }
+
+    // verifica se um input passou do límite de caracteres
+    maxlength(input, maxValue){
+
+        let inputLength = input.value.length;
+
+        let errorMessage = `o campo precisa ter menos que  ${maxValue} caracteres`;
+
+        if (inputLength > maxValue) {
+            this.printMessage(input, errorMessage);
+        }
+
+    }
+
+    // método para imprimir mensagens de erro na tela 
+    printMessage(input, msg){
+
+        let template = document.querySelector(".error-validation").cloneNode(true);
+
+        template.textContent = msg;
+
+        let inputParent = input.parentNode;
+
+        template.classList.remove("template");
+
+        inputParent.appendchild(template);
+
+    }
+
+    // verifica se o input é requerido
+
+    //limpa as validações da tela 
+    cleanValidations(validations){
+        validations.forEach(el => el.remove());
     }
 }
 
 let form = document.getElementById("register_form");
 let submit = document.getElementById("btn_submit");
+
+let validator = new validator();
+
+// evento que dispara as validações
+submit.addEventListener("click", function(e)){
+
+}
